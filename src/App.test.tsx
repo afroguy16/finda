b5fb726fe16cc8cbb14f8600eed93959919766ee
afroguy16 <tega_joe@hotmail.com>
2022-04-29 20:34:58 +0000

    init app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, cleanup } from '@testing-library/react';
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Tag', () => {
+  const { baseElement } = render(<App />)
+
+    afterAll(cleanup);
+
+    it('should render successfully', () => {
+        expect(baseElement).toBeTruthy()
+    });
+})
