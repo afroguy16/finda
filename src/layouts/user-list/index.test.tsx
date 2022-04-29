bb24ca4b51020df27261520541f6199e984525ee
afroguy16 <tega_joe@hotmail.com>
2022-04-29 22:22:32 +0000

    feat: [setup] add UserList Component

---
+++ src/layouts/user-list/index.test.tsx
@@ -0,0 +1,30 @@
+import { render, cleanup, screen } from "@testing-library/react";
+import UserList from ".";
+import { users } from "../../mocks/users";
+import { UserT } from "../../types/user";
+
+describe("User List", () => {
+  let baseElement: HTMLElement;
+  const fakeUsers: Array<UserT> = [ ...users ];
+
+  afterEach(cleanup);
+
+  beforeEach(() => {
+    const utils = render(
+      <UserList users={fakeUsers} />
+    );
+    baseElement = utils.baseElement;
+  });
+
+  it("should render successfully", () => {
+    expect(baseElement).toBeTruthy()
+  });
+
+  it("should render a list of users that is passed through the props", () => {
+    const userListElement = screen.getByRole("list");
+    const userElement = screen.getAllByRole('listitem')
+
+    expect(userListElement).toBeTruthy()
+    expect(userElement).toHaveLength(fakeUsers.length)
+  });
+});
---
+++ src/layouts/user-list/index.tsx
@@ -0,0 +1,36 @@
+import User from "../../components/user";
+import { UserT } from "../../types/user";
+import styles from "./user.module.scss";
+
+type Props = {
+  users: UserT[];
+};
+
+const UserList = (props: Props) => {
+  const { users } = props;
+
+  const getUserElements = (users: Array<UserT>) =>
+    users.map((user) => (
+      <li key={user.id}>
+        <User
+          login={user.login}
+          type={user.type}
+          avatar_url={user.avatar_url}
+        />
+      </li>
+    ));
+
+  return (
+    <div className={styles.wrapper} data-testid="wrapper">
+      <ul>
+        {users && users.length > 0 ? (
+          getUserElements(users)
+        ) : (
+          <p>No user found</p>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
