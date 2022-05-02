dbecb5ba90e0325a5357a70cb77cf579ba775230
afroguy16 <tega_joe@hotmail.com>
2022-05-02 10:09:01 +0000

    fix [fix-failing-test] add failing tests]

--- src/components/pagination/index.test.tsx
+++ src/components/pagination/index.test.tsx
@@ -24,4 +24,14 @@ describe("Pagination", () => {
   it("should render successfully", () => {
     expect(baseElement).toBeTruthy();
   });
+
+  it("should call onSelect call back with the selected page number if a page is selected", () => {
+    const listItemIndex = 4;
+    const pageNumber = 3;
+    const thirdListItem = screen.getAllByRole("listitem")[listItemIndex];
+
+    fireEvent.click(thirdListItem)
+
+    expect(mockOnSelectPageCallBack).toHaveBeenCalledWith(pageNumber); //not passing, unknown reason, suspect is library compatibility because this works in the dom. I will have to confirm. I am leaving this hear so I can get back to it
+  });
 });
