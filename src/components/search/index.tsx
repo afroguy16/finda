90c2ca794bc23a1f775cbed20e0ad38515d01460
afroguy16 <tega_joe@hotmail.com>
2022-05-04 16:59:55 +0000

    feat: [login-sorting] move function declaration to useEffect

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -16,22 +16,24 @@ const [searchValue$, setSearchValue] = createSignal<any>();
 
 const Search = ({ onChange, placeholder, loading }: Props) => {
   const [alive, setAlive] = useState(true);
-  useEffect(() => {
-    subscribeToSearchValueChange();
-    return () => setAlive(false);
-  }, []);
 
-  const subscribeToSearchValueChange = () => {
-    searchValue$
-      .pipe(
-        debounceTime(DELAY_INPUT_CHANGE),
-        distinctUntilChanged(),
-        takeWhile(() => alive)
-      )
-      .subscribe((value) => {
-        value && onChange(value);
-      });
-  };
+  useEffect(() => {
+    const subscribeToSearchValueChange = () => {
+      searchValue$
+        .pipe(
+          debounceTime(DELAY_INPUT_CHANGE),
+          distinctUntilChanged(),
+          takeWhile(() => alive)
+        )
+        .subscribe((value) => {
+          value && onChange(value);
+        });
+    };
+
+    subscribeToSearchValueChange();
+
+    return () => setAlive(false);
+  }, [alive, searchValue$]);
 
   const emitNewSearchValue = (newValue: string) => {
     setSearchValue(newValue);
